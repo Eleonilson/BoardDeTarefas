8d9d189597dedd574305850506393eb69a835c9e
Eleonilson <eleonilson.r.l@gmail.com.br>
2025-04-08 11:30:34 -0300

    Criando migrações

---
+++ src/main/resources/db/changelog/migrations/db.changelog-202504081031.sql
@@ -0,0 +1,14 @@
+--liquibase formatted sql
+--changeset junior:202504081031
+--comment: cards table create
+
+CREATE TABLE CARDS(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    `order` int NOT NULL,
+    board_columns_id BIGINT NOT NULL,
+    CONSTRAINT boards_columns__cards_fk FOREIGN KEY (board_columns_id) REFERENCES BOARDS_COLUMNS(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+--rollback DROP TABLE CARDS
